06fcc401fbd2025aa3c72332de6144a7cab2dc23
Tefariv <91168046+Tefariv@users.noreply.github.com>
2023-09-24 11:44:26 -0500

    Add files via upload

---
+++ Ejercicio_practico5.py
@@ -0,0 +1,9 @@
+x=0
+y=1
+
+while x< 1597:
+    print(x, y, end=" ")
+    x+=y
+    y+=x
+
+        
---
+++ format.py
@@ -0,0 +1,16 @@
+#uso de format 
+nombre="Stephanie"
+edad=17
+
+print ("Hola {} tienes {} años".format(nombre, edad))
+
+#2 forma
+
+print("Hola {nombre} tines {años} años".format(nombre="Tefa", años=17))
+
+#3 forma
+nombre="Stephanie"
+edad=17
+
+print("hola {1} tienes {0} años".format(edad, nombre))
+
