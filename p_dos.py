95292b4fe7c7f9ea1363a8ec98d9d75d4f626a79
Tefariv <91168046+Tefariv@users.noreply.github.com>
2023-11-23 23:46:11 -0500

    Actualizar p_dos.py

--- p_dos.py
+++ p_dos.py
@@ -2,6 +2,21 @@ compra =input (int("ingrese el valor de su compra"))
 if compra <1000:
     print(compra)
     
+    if compra < 5000:
+        compra= compra-(compra*0.05)
+        print(compra)
+    elif compra < 7000:
+        compra= compra-(compra*0.15)
+        print(compra)
+    elif compra < 10000:
+         compra= compra-(compra*0.15)
+        print(compra)
+    if else 
+        
+    
+    
+    
+    
 else:
     print("El valor es incorrecto")
     
