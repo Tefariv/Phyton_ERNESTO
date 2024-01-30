a7f1d2b16b68f6578df229e6f3661798d036a1a9
Tefariv <91168046+Tefariv@users.noreply.github.com>
2024-01-30 06:16:59 -0500

    Actualizar suma.py

--- suma.py
+++ suma.py
@@ -3,3 +3,10 @@ numero_uno= 2
 numero_dos= 4
 resultado= numero_uno + numero_dos
 print(resultado)
+
+
+
+
+#programa cap 12
+print("===============================")
+print("convertidor de numeros a letras")
