322ffecfbe0d581ae159c86f489c8b0573f6b52f
Tefariv <91168046+Tefariv@users.noreply.github.com>
2024-01-30 08:41:06 -0500

    Actualizar suma.py

--- suma.py
+++ suma.py
@@ -83,12 +83,20 @@ print("\n introduce dos numeros a comparar: \n")
 num_uno=int(input("introduce el primer numero: ")
 num_dos=int(input("introduce el segundo numero: ")
 
+print("los numeros a editar son: ", num_uno " y ", num_dos)
+
 if num_uno != num_dos:
     print("es diferente...")
-elif num_uno > num_dos:
+if num_uno == num_dos:
+    print("son iguales...")
+if num_uno > num_dos:
     print("es mayor...")
-elif num_uno >= num_dos:
+if num_uno >= num_dos:
     print("Es mayor o igual...")
+if num_uno < num_dos:
+    print("es menor...")
+if num_uno <= num_dos:
+    print("Es menor o igual...")
 
 
         
