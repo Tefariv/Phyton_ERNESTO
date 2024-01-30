a02643eb5a41369b77be414694c9f8f81993bf11
Tefariv <91168046+Tefariv@users.noreply.github.com>
2024-01-30 08:10:05 -0500

    Actualizar suma.py

--- suma.py
+++ suma.py
@@ -32,14 +32,14 @@ else:
     #condicionales anidadas
 print("=========")
 print("conversor")
-print("=========")
-print(" ")
+print("\n=========\n")
 print("menu de opciones)
 
 opcion_uno=int(input("Elige uno para pasar de numero a letra: "))
 opcion_dos=int(input("Elige dos para pasar de letra a numero: "))
 
 if opcion_uno==1:
+print("converosr de numero a letra")
     dato_uno=int(input("cual es el numero que deseas pasar a palabra? :"))
     if dato_uno==1:
         print("el numero es 'uno'")
@@ -51,18 +51,25 @@ if opcion_uno==1:
         print("El numero es 'cuatro'")
     elif dato_uno == 5:
         print("El numero es 'cinco'")
+    elif:
+        print("el numero no esta registrado"$
         
 elif opcion_uno==2:
-    dato_dos=int(input("cual es el numero que deseas pasar a palabra? :"))
-    if dato_dos==1:
-        print("el numero es 'uno'")
-    elif dato_dos == 2:
-        print("El numero es 'dos'")
-    elif dato_dos == 3:
-        print("El numero es 'tres'")
-    elif dato_dos == 4:
-        print("El numero es 'cuatro'")
-    elif dato_dos == 5:
-        print("El numero es 'cinco'")
+print("conversor de numero a palabra")
+    dato_dos=str(input("cual es la palabra qur deseas pasar a numero? :"))
+    if dato_dos==uno:
+        print("el numero es '1'")
+    elif dato_dos == dos:
+        print("El numero es '2'")
+    elif dato_dos == tres:
+        print("El numero es '3'")
+    elif dato_dos == cuatro:
+        print("El numero es '4'")
+    elif dato_dos == cinco:
+        print("El numero es '5'")
+    elif:
+        print("la palabra no esta registrado")
+else:
+    print("Fin")
         
         
