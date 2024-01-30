d684527be9c86acd90d1ab457f5374422f6eff37
Tefariv <91168046+Tefariv@users.noreply.github.com>
2024-01-30 06:22:41 -0500

    Actualizar suma.py

--- suma.py
+++ suma.py
@@ -10,3 +10,21 @@ print(resultado)
 #programa cap 12
 print("===============================")
 print("convertidor de numeros a letras")
+print("===============================")
+
+num=int(input("caul es l numero que deseas convertir? :"))
+
+if num == 1:
+    print("El numero es 'Uno'")
+elif num == 2:
+    print("El numero es 'dos'")
+elif num == 3:
+    print("El numero es 'tres'")
+elif num == 4:
+    print("El numero es 'cuatro'")
+elif num == 5:
+    print("El numero es 'cinco'")
+    
+    
+else:
+    print("este programa solo convierte hasta el numero 5")
