018de3411baf62566ccccfc096cdb23cf7864d03
Tefariv <91168046+Tefariv@users.noreply.github.com>
2024-05-31 08:26:50 -0500

    Update End.py

--- End.py
+++ End.py
@@ -13,3 +13,16 @@ print ("hola ",()," tienes ", (), " años".format(nombre, edad))
 
 print("Hola ",(nombre)," tienes ",(edad), " años".format(nombre="Tefa", edad=17)
 
+
+z=0
+a=["a", "b", "c", "a"]
+for x in a:
+ print(z)
+z=z+1
+
+
+a=[0,1,2,3,4,5]
+c=0
+b=a[c]
+while b>0:
+ print(a[c-2])
